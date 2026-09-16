faaf156a3f691d7e4b78aef3f0dd20e278e14699
Nicole Barajas <nicole.azpillaga@gmail.com>
2026-09-15 20:53:51 -0400

    Add multi-page query for top events by genre
    This SQL query provides a sales summary of the top-performing event for each entertainment genre over a specified date range, enabling performance comparison and data-driven decision-making.

--- SQL_Queries/Top_Event_by_Genre_Query.sql
+++ SQL_Queries/Top_Event_by_Genre_Query.sql
@@ -1,1 +1,52 @@
+/*This multi-page query provides a sales summary of the top-performing event for each of the company's 21 entertainment genres, ranging from Country and Rock/Pop to Comedy and Family shows, for a specified date range. The report enabled business leaders to compare event performance across genres, identify high-revenue attractions, and make data-driven decisions regarding future event scheduling, promotions, and booking strategies.
+*/
+
+Select *
+From (Select LAT.Genre,
+Sales_and_Collections.Event,
+Sales_and_Collections.Net_Sales
+From Sales_and_Collections JOIN LAT on LAT.Act=Sales_and_Collections.Event
+Where LAT.Genre = 'Broadway'
+and LAT.Event_Start between '2025-07-01' and '2026-03-31'
+Order By Sales_and_Collections.Net_Sales desc
+Limit 1)Broadway_Top_Event
+
+UNION
+
+
+Select *
+From (Select LAT.Genre,
+Sales_and_Collections.Event,
+Sales_and_Collections.Net_Sales
+From Sales_and_Collections JOIN LAT on LAT.Act=Sales_and_Collections.Event
+Where LAT.Genre = 'Bluegrass/Folk'
+and LAT.Event_Start between '2025-07-01' and '2026-03-31'
+Order By Sales_and_Collections.Net_Sales desc
+Limit 1)Broadway_Top_Event
+
+UNION
+
+Select *
+From (Select LAT.Genre,
+Sales_and_Collections.Event,
+Sales_and_Collections.Net_Sales
+From Sales_and_Collections JOIN LAT on LAT.Act=Sales_and_Collections.Event
+Where LAT.Genre = 'Cabaret/Variety'
+and LAT.Event_Start between '2025-07-01' and '2026-03-31'
+Order By Sales_and_Collections.Net_Sales desc
+Limit 1)Broadway_Top_Event
+
+UNION
+
+Select *
+From (Select LAT.Genre,
+Sales_and_Collections.Event,
+Sales_and_Collections.Net_Sales
+From Sales_and_Collections JOIN LAT on LAT.Act=Sales_and_Collections.Event
+Where LAT.Genre = 'Gospel'
+and LAT.Event_Start between '2025-07-01' and '2026-03-31'
+Order By Sales_and_Collections.Net_Sales desc
+Limit 1)Broadway_Top_Event
+
+
 
